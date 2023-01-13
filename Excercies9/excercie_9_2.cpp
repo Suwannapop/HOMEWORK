8abf293155f670c7b699f012da7afb975b3eb278
Suppakron <suwannapop01@gmail.com>
2023-01-13 21:12:05 +0700

    oky

--- Excercies9/excercie_9_2.cpp
+++ Excercies9/excercie_9_2.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std ;
 int main (){
-  ofstream score ;
-  score.open("score.txt");
-  score << "Check" ;
+    string input;
+    double find = 0 , count=0, min , max;
+    ifstream score ;
+    score.open("score.txt");
+    do
+    {
+        cout << "Enter range of the scores [min,max] for counting: ";
+        cin >> min >> max;
+        if (min > max )
+        {
+            cout << "Invalid input. (max>=min)\n";
+        }
+
+        
+    } while (max < min);
+    
+    
+    while (getline(score , input))
+    {
+       find = atof(input.c_str()) ;
+       if (min <= find and max >= find )  
+       {
+         count++ ;
+       }
+    }
+    cout << "The number of students who have scores in this range = " << count ;
+    
+    
+    
 }
