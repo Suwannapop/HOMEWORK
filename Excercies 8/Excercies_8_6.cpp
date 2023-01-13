a39cd300eafb68dc424feb9f6c99591d52b0103b
Suppakron <suwannapop01@gmail.com>
2023-01-13 20:36:02 +0700

    okey

---
+++ Excercies 8/Excercies_8_6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std ;
+int main (){
+    int N ;
+    cout << "Please input board size : ";
+    cin >> N ;
+    for (int i = 1  ; i <= N; i++)
+    {
+        for (int j = 1; j < N; j++)
+        {
+            cout << "   ";
+            if (j < N )
+            {
+                cout << "|";
+            }
+            
+        }
+        cout << endl ;
+        if (i < N)
+        {
+            for (int j = 1; j <= N; j++)
+            {
+                cout << "---";
+                if (j < N )
+                {
+                    cout << "+";
+                }
+            }
+            cout << endl;
+            
+        }
+       
+    }
+    
+}
