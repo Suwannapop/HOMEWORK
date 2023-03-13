9fff7fa43550148f5b559a6ae8bb6e77a7d9f1c5
Suppakorn <suwannapop01@gmail.com>
2023-03-13 23:22:48 +0700

    good

--- Excercies 13/Excerices13_2.cpp
+++ Excercies 13/Excerices13_2.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 using namespace std ;
 
+void randomMatrix(long double A[][10]){
+    int i , j , result;
+    i = rand()%10 + 1 ;
+    j = rand()%10 + 1 ;
+    cout << "\n\nrandomMatrix :: ";
+    cout << A[i][j];
+    cout << "\n locatoin is A[" << i <<"][" << j <<"]";
+
+}
+
 int main (){
+    srand(time(0));
+   long double A[10][10] ;
+   for (int i = 0; i < 10; i++)
+   {
+    for (int j = 0; j < 10; j++)
+    {
+        long double result = rand()%10000/1000.0;
+        A[i][j] = result ;
+    }
+    
+   }
+   for (int i = 0; i < 10; i++)
+   {
+    for (int j = 0; j < 10; j++)
+    {
+        cout <<setprecision(4)<< setw(8)<< A[i][j] << setw(8) ;
+    }
+    cout << endl ;
+   }
+   randomMatrix(A);
    
 }
